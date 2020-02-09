ee620d893dda28527062ab86e5c7f5fcb79cfbf0
yeonghyeon <hah_528@naver.com>
2020-02-09 19:13:59 +0900

    first commit

---
+++ src/main/java/com/yeonghyeon/pmboard/pmboard/PmboardApplication.java
@@ -0,0 +1,13 @@
+package com.yeonghyeon.pmboard.pmboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeonghyeon/pmboard/pmboard/PmboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonghyeon.pmboard.pmboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
